6ecc7e32e536b57a2cc772a3fe43b786b60d32d3
carlosaqf <carlosfegurgur@gmail.com>
2022-02-03 20:43:38 -0600

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app-container">
+        <h1 className="app-title">
+          <span className="app-title-top">S P L I</span>
+          <br />
+          <span className="app-title-bottom">T T E R</span>
+        </h1>
+
+        <div className="app-form-container">
+          <form className="app-form">
+
+            <label htmlFor="bill">Bill</label>
+            <input type="number" id="bill" />
+
+            <label htmlFor="tip-button-group">Select Tip %</label>
+            <div className="tip-button-group" id="tip-button-group">
+              <button>5%</button>
+              <button>10%</button>
+              <button>15%</button>
+              <button>25%</button>
+              <button>50%</button>
+              <input type="number" />
+            </div>
+
+            <label htmlFor="people-count">Number of People</label>
+            <input type="number" id="people-count" />
+
+            <div className="amount-total-container">
+              <p>Tip Amount / person</p>
+              <h4>$4.27</h4>
+              <p>Total / person</p>
+              <h4>$32.79</h4>
+              <button className="reset-button">Reset</button>
+            </div>
+          </form>
+        </div>
+      </div> 
+
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="https://www.github.com/carlosaqf" target="_blank" rel="noreferrer">Carlos Fegurgur</a>.
+      </footer>
+    </>
   );
 }
 
