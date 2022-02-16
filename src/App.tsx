77d6a1365197858dccbcec5aa5d8c05bcb8844e0
carlosaqf <carlosfegurgur@gmail.com>
2022-02-15 20:51:59 -0600

    Added onClick and onChange functions.

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,36 @@ const App = () => {
   
   const [value, setValue] = useState();
 
-  const [billValue, setBillValue] = useState<number>(200);
+  const [billValue, setBillValue] = useState<number>(0);
   const [numPeople, setNumPeople] = useState<number>(5);
   const [tipPerPerson, setTipPerPerson] = useState<number>(0);
   const [totalPerPerson, setTotalPerPerson] = useState<number>(0);
+  const [tipPercentage, setTipPercentage] = useState<number>();
 
-  const handleTipPerPerson = (tipPercentage: number) => {
-    const tipAmount = billValue * (tipPercentage / 100);
-    console.log(tipAmount);
-    setTipPerPerson(tipAmount);
+  const handleButtonClick = (e: any) => {
+    e.preventDefault();
+    setTipPercentage(Number(e.target.value));
+
+    const allButtons = e.target.parentElement.querySelectorAll('#tip-button');
+
+    allButtons.forEach((button: any) => {
+      if (e.target === button){
+        e.target.classList.add('active');
+      }else if (button.classList.contains('active')){
+        button.classList.remove('active');
+      }
+    })
+
+  }
+
+  const handleInputChange = (e: any) => {
+    if (e.target.id === 'bill'){
+      setBillValue(Number(e.target.value));
+    }else if (e.target.id === 'people-count'){
+      setNumPeople(Number(e.target.value));
+    }else if (e.target.id === 'custom-tip'){
+      setTipPercentage(Number(e.target.value));
+    }
   }
 
   return (
@@ -42,22 +63,20 @@ const App = () => {
               id="bill"
               label="Bill"
               svg="iconDollar"
+              onChange={(e: any) => handleInputChange(e)}
             />
 
             <ButtonGroup label="Select Tip %" id="tip-button-group">
-              <Button 
-                text="5%" 
-                value={5}
-                handleTipPerPerson={handleTipPerPerson}
-              />
-
-              <Button text="10%" value={10} handleTipPerPerson={handleTipPerPerson}/>
-              <Button text="15%" value={15} handleTipPerPerson={handleTipPerPerson}/>
-              <Button text="25%" value={25} handleTipPerPerson={handleTipPerPerson}/>
-              <Button text="50%" value={50} handleTipPerPerson={handleTipPerPerson}/>
+              <Button text="5%" value={5} onClick={(e) => handleButtonClick(e)} id="tip-button" />
+              <Button text="10%" value={10} onClick={(e) => handleButtonClick(e)} id="tip-button" />
+              <Button text="15%" value={15} onClick={(e) => handleButtonClick(e)} id="tip-button" />
+              <Button text="25%" value={25} onClick={(e) => handleButtonClick(e)} id="tip-button" />
+              <Button text="50%" value={50} onClick={(e) => handleButtonClick(e)} id="tip-button" />
               <Input 
                 type="number"
                 placeholder="Custom"
+                id="custom-tip"
+                onChange={(e: any) => handleInputChange(e)}
                 />
             </ButtonGroup>
 
@@ -66,13 +85,15 @@ const App = () => {
               label="Number of People"
               id="people-count"
               svg="iconPerson"
+              onChange={(e: any) => handleInputChange(e)}
             />
 
-            <Amount 
-              tipPerPerson={tipPerPerson}
-              totalPerPerson={totalPerPerson}
+            <Amount
+              billValue={billValue}
+              numPeople={numPeople}
+              tipPercentage={tipPercentage}
             >
-              <Button text="Reset" inverse handleTipPerPerson={handleTipPerPerson}/>
+              <Button text="Reset" inverse onClick={(e) => handleButtonClick(e)} />
             </Amount>
           </form>
         </div>
